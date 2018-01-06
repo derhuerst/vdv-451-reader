33250e40cedf531038ee4057ccd723ca6b73831f
Jannis R <mail@jannisr.de>
2018-01-06 19:55:27 +0100

    bugfix :bug:, put things together

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-// todo
+const duplexer = require('duplexer3')
+
+const createFileReader = require('./lib/file-reader')
+const createFileParser = require('./lib/file-parser')
+
+const createReader = () => {
+	const input = createFileReader()
+	const output = createFileParser()
+
+	input.pipe(output)
+	return duplexer({objectMode: true}, input, output)
+}
+
+module.exports = createReader
